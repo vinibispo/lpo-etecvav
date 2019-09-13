ff7271056ebef50f3a6ed1505487df6d2ce3ac73
vinibispo <vini.bispo015@gmail.com>
2019-09-13 16:04:25 -0300

    almost done

--- Projeto/ProjetoForm1/ProjetoForm1/ManipulacaoString.cs
+++ Projeto/ProjetoForm1/ProjetoForm1/ManipulacaoString.cs
@@ -12,7 +12,7 @@ namespace ProjetoForm1
 {
     public partial class ManipulacaoString : Form
     {
-        int a, lettere, i, o, u;
+        int a, lettere, letteri, o, u;
         string phrase, inverse, space;
         string[] words;
 
@@ -53,16 +53,40 @@ namespace ProjetoForm1
             inverse = "";
             for (int i = 0; i < phrase.Length; i++)
             {
+                if(phrase.ToUpper().Substring(i, 1) == "A")
+                {
+                    a += 1;
+                }
+                if (phrase.ToUpper().Substring(i, 1) == "E")
+                {
+                   lettere += 1;
+                }
+                if (phrase.ToUpper().Substring(i, 1) == "I")
+                {
+                    letteri += 1;
+                }
+                if (phrase.ToUpper().Substring(i, 1) == "O")
+                {
+                    o += 1;
+                }
+                if (phrase.ToUpper().Substring(i, 1) == "U")
+                {
+                    u += 1;
+                }
                 lb_letter.Items.Add(phrase[i]);
             }
             txt_char.Text = phrase.Length.ToString();
             txt_a.Text = a.ToString();
             txt_e.Text = lettere.ToString();
-            txt_i.Text = i.ToString();
+            txt_i.Text = letteri.ToString();
             txt_o.Text = o.ToString();
             txt_u.Text = u.ToString();
             txt_together.Text = phrase.Replace(" ", "");
-            words = txt_fra.Text.Split();
+            words = txt_fra.Text.Split(' ');
+            for(int i = 0; i< words.Length; i++)
+            {
+                lb_word.Items.Add(words[i]);
+            }
             
         }
     }
